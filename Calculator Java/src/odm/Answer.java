feaa870030dd4c0bfdae786f51184e00f5b74ba2
odmguy <odmguy@gmail.com>
2025-07-24 09:49:31 +0100

    Initial Version

---
+++ Calculator Java/src/odm/Answer.java
@@ -0,0 +1,7 @@
+package odm;
+
+public class Answer {
+	public double result;
+	public boolean hasErrors = false;
+	public java.util.Vector<String> messages = new java.util.Vector<String>();
+}
---
+++ Calculator Java/src/odm/Calculator.java
@@ -0,0 +1,7 @@
+package odm;
+
+public class Calculator {
+	public double arg1;
+	public double arg2;
+	public String operator; // + / - x*
+}
